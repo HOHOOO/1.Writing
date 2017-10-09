4f77dc3d6eac177811bb9b5a4ecbc107ca1d37ab
houzhiwei <houzhiwei@smzdm.com>
2017-10-09 15:55:01 +0800

    1

--- WorkStation/rsync_nginx_log.sh
+++ WorkStation/rsync_nginx_log.sh
@@ -6,34 +6,34 @@ day=`date -d "$1 day ago" +"%d"`
 data=`date -d "$1 day ago" +"%Y%m%d"`
 echo ${data}
 home_path=$2
-rm -rf $home_path/nginx/*
-rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/nginx/
+rm -rf $home_path/file/nginx/*
+rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/file/nginx/
 
-DIRECTORY="$home_path/nginx"
+DIRECTORY="$home_path/file/nginx"
 if [ "`ls -A $DIRECTORY`" != "" ]; then
     echo "rsync成功"
     hadoop dfs -mkdir -p /dataOffline/nginx/$year/$month/$day
-    hadoop dfs -put $home_path/nginx/* /dataOffline/nginx/$year/$month/$day/
+    hadoop dfs -put $home_path/file/nginx/* /dataOffline/nginx/$year/$month/$day/
 else
     sleep 300
     echo "重启rsync"
-    rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/nginx/
+    rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/file/nginx/
     if [ "`ls -A $DIRECTORY`" != "" ]; then
         echo "rsync成功"
         hadoop dfs -mkdir -p /dataOffline/nginx/$year/$month/$day
-        hadoop dfs -put $home_path/nginx/* /dataOffline/nginx/$year/$month/$day/
+        hadoop dfs -put $home_path/file/nginx/* /dataOffline/nginx/$year/$month/$day/
     else
         sleep 300
         echo "重启rsync"
-        rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/nginx/
+        rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/file/nginx/
         if [ "`ls -A $DIRECTORY`" != "" ]; then
              echo "rsync成功"
              hadoop dfs -mkdir -p /dataOffline/nginx/$year/$month/$day
-             hadoop dfs -put $home_path/nginx/* /dataOffline/nginx/$year/$month/$day/
+             hadoop dfs -put $home_path/file/nginx/* /dataOffline/nginx/$year/$month/$day/
         else
              sleep 300
              echo "重启rsync"
-             rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/nginx/
+             rsync -vrtl --progress --password-file=/home/hadoop/.http.access.log.password readAccessLogUser@10.9.42.154::http-access-log/*/*.http.access.log.${data}_* $home_path/file/nginx/
         fi
    fi
 fi
