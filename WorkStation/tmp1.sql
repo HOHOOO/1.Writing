03c1a70e243a135dd484dee7c40c3ae82402fa12
houzhiwei <houzhiwei@smzdm.com>
2017-10-24 14:32:15 +0800

    1

--- WorkStation/tmp1.sql
+++ WorkStation/tmp1.sql
@@ -31,52 +31,7 @@ CREATE TABLE recommend.tag_relation_count_cross AS SELECT t.tag_id_1,t.tag_id_2,
 select "标签之间相似度";
 CREATE TABLE recommend.tag_relation_collaborative AS SELECT t1.tag_id_1 AS tag_id_1,t2.user_num_1 AS user_num_1,t1.tag_id_2 AS tag_id_2,t3.user_num_2 AS user_num_2,t1.num AS num,(t1.num/sqrt(t2.user_num_1*t3.user_num_2)) AS power,row_number () over (ORDER BY (t1.num/sqrt(t2.user_num_1*t3.user_num_2)) DESC) rank FROM recommend.tag_relation_count_cross t1 LEFT JOIN (SELECT tag_id,user_num AS user_num_1 FROM recommend.tag_relation_count_left) t2 ON t1.tag_id_1=t2.tag_id LEFT JOIN (SELECT tag_id,user_num AS user_num_2 FROM recommend.tag_relation_count_right) t3 ON t1.tag_id_2=t3.tag_id GROUP BY t1.tag_id_1,t2.user_num_1,t1.tag_id_2,t3.user_num_2,t1.num,(t1.num/sqrt(t2.user_num_1*t3.user_num_2));
 
-CREATE TABLE recommend.tag_relation_collaborative_top10 AS SELECT tag_id_1,tag_id_2,power FROM (SELECT tag_id_1,tag_id_2,power,row_number () over (PARTITION BY tag_id_1 ORDER BY power DESC) row_id FROM recommend.tag_relation_collaborative) t1 WHERE t1.row_id<10;
+CREATE TABLE recommend.tag_relation_collaborative_top20 AS SELECT concat_ws('_',"cate",tag_id_1) as tag_id_1,concat_ws('_',"cate",tag_id_2) as tag_id_2,power,rank FROM (SELECT tag_id_1,tag_id_2,power,row_number () over (PARTITION BY tag_id_1 ORDER BY power DESC) rank FROM recommend.tag_relation_collaborative) t1 WHERE t1.rank<20;
 
-
-
-
-
-
-
-create table recommend.tag_relation_collaborative
-    as
-    select t1.tag_id_1 as ta_id_1,
-       t2.user_num_1 as user_num_1,
-       t1.tag_id_2 as tag_id_2,
-       t3.user_num_2 as user_num_2,
-       t1.num as num,
-       (t1.num/sqrt(t2.user_num_1 * t3.user_num_2)) as power,
-       row_number() over(order by (t1.num/sqrt(t2.user_num_1 * t3.user_num_2)) desc) rank
-      from recommend.tag_relation_count_cross t1
-   left join (select tag_id,
-              user_num as user_num_1
-         from recommend.tag_relation_count_left
-         ) t2
-      on t1.tag_id_1 = t2.tag_id
-   left join (select tag_id,
-              user_num as user_num_2
-         from recommend.tag_relation_count_right
-         ) t3
-      on t1.tag_id_2 = t3.tag_id
-    group by t1.ta_id_1,
-        t2.user_num_1,
-        t1.tag_id_2,
-        t3.user_num_2,
-        t1.num,
-            (t1.num/sqrt(t2.user_num_1 * t3.user_num_2))
-
-
-
-            create table recommend.tag_relation_collaborative_top10
-              as
-              select tag_id_1,
-            		 tag_id_2,
-            		 power
-            	from (select tag_id_1,
-            				 tag_id_2,
-            				 power,
-            				 row_number() over(partition by tag_id_1 order by power desc) row_id
-            			from recommend.tag_relation_collaborative
-            			) t1
-            			where t1.row_id=1
+create table recommend.tag_relation_collaborative_top20_name as select d.*,c.tag_name from recommend.dw_cp_dic_tag_info c right join  (select a.*,b.tag_name as tag_relation from recommend.tag_relation_collaborative_top20 a left join recommend.dw_cp_dic_tag_info b on a.tag_id_1=b.tag_id and b.ds='2017-10-23') d on c.tag_id=d.tag_id_2 and c.ds='2017-10-23';
+create table recommend.tag_relation_collaborative_top10_name_end as select a.*,b.tag_name  from recommend.tag_relation_collaborative_top10_name a left join  b on a.user_tag_2=b.tag_id and b.ds='2017-10-23';
