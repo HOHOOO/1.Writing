7b3cd6545c0d29b9368c7b90d05f1ac24daa3e0e
houzhiwei <houzhiwei@smzdm.com>
2017-10-26 11:49:22 +0800

    1

--- WorkStation/quality_score_test.sh
+++ WorkStation/quality_score_test.sh
@@ -3,24 +3,9 @@ home_path=$(cd "`dirname $0`"; pwd)
 quality_sync_time=`date -d "4 day ago" +"%Y-%m-%d %H:%M:%S"`
 score_timestamp=`date -d "now" +%s`
 
-#input para
-zscore=$1
-yuanchuang_para=$2
-yuanchuang_para2=$3
-yuanchuang_para3=$4
-yuanchuang_para4=$5
-store_decrease=$6
-echo $zscore
-echo $yuanchuang_para1
-echo $yuanchuang_para2
-echo $yuanchuang_para3
-echo $yuanchuang_para4
-echo $store_decrease
-
-
-hive -e '
+hive -d zscore='1.96' -d  yuanchuang_para='1' -d yuanchuang_para2='1' -d yuanchuang_para3='1' -d yuanchuang_para4='1' -d store_decrease='0.8' -e '
 use recommend;
 
-SELECT "$zscore,$yuanchuang_para1,$yuanchuang_para2,$yuanchuang_para3,$yuanchuang_para4,$store_decrease";
+SELECT "${zscore},$yuanchuang_para1,$yuanchuang_para2,$yuanchuang_para3,$yuanchuang_para4,$store_decrease";
  SELECT id, (pow( $yuanchuang_para ,1) * collection_count from  ( SELECT id,collection_count,love_rating_count,comment_count,reward_count,$score_timestamp  as score_timestamp FROM sync_yuanchuang ) limit 5;
 '
