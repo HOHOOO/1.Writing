dc3e281cea45a0c1a25b4a2e129003957065524e
houzhiwei <houzhiwei@smzdm.com>
2017-10-26 15:28:37 +0800

    1

--- WorkStation/quality_score.sh
+++ WorkStation/quality_score.sh
@@ -46,10 +46,8 @@ CREATE TABLE recommend.quality_data_source_wilson AS SELECT id, CASE WHEN phat=-
 CREATE TABLE recommend.quality_data_source AS SELECT a.* from (select * from recommend.quality_data_source_newton union all  select * from recommend.quality_data_source_wilson) a;
 
 INSERT OVERWRITE TABLE recommend.quality_data_score
-Select b.id,b.score,b.last_status,CASE WHEN last_status=0 THEN 0 ELSE (score - last_status)/last_status END AS increase_rate ,b.order_rank,b.score_timestamp from (
-      select id, score,score_timestamp,lag(score,1, 0) over (partition by a.id order by a.score_timestamp ASC) as last_status ,rank() over (partition by id order by score_timestamp desc) as order_rank
-      from (select id, score,score_timestamp from recommend.quality_data_score WHERE order_rank=1
-            union all
-           select id, score,score_timestamp  from recommend.quality_data_source ) a
-) b;
+select id, score,score_timestamp,lag(score,1, 0) over (partition by id order by score_timestamp ASC) as last_status ,rank() over (partition by id order by score_timestamp desc) as order_rank
+from (select id, score,score_timestamp from recommend.quality_data_score WHERE order_rank=1
+      union all
+     select id, score,score_timestamp  from recommend.quality_data_source ) a;
 '
